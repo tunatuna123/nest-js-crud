6cde0377a65c851c888988d987c37c56d432c799
tunatuna123 <tuna200538@gmail.com>
2024-02-22 00:42:17 +0900

    Feat: "Product controller"

---
+++ src/product/product.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.model';
+
+export class BookController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAllProduct(): Promise<Product[]> {
+    return this.productService.getAllProduct();
+  }
+
+  @Get(':id')
+  async getProduct(@Param('id') id: number): Promise<Product | null> {
+    return this.productService.getProduct(id);
+  }
+
+  @Post()
+  async createProduct(@Body() postData: Product): Promise<Product> {
+    return this.productService.createProduct(postData);
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: number): Promise<Product> {
+    return this.productService.deleteProduct(id);
+  }
+
+  @Put(':id')
+  async updateProduct(
+    @Body() postData: Product,
+    @Param('id') id: number,
+  ): Promise<Product> {
+    return this.productService.updateProduct(id, postData);
+  }
+}
