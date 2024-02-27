764c6706b543424d4e70a3528fe1af853ffff6f1
tunatuna123 <tuna200538@gmail.com>
2024-02-27 19:11:26 +0900

    chore: prettier

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,9 +1,9 @@
-import { Prisma } from "@prisma/client";
+import { Prisma } from '@prisma/client';
 
 export class Product implements Prisma.productCreateInput {
-    id: number;
-    name: string;
-    quantity: number;
-    price: number;
-    image?: string | null | undefined;
+  id: number;
+  name: string;
+  quantity: number;
+  price: number;
+  image?: string | null | undefined;
 }
